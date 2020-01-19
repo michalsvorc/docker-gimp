40710bd390da68a782ac2514e2ba16fc7ca421f8
Michal Svorc <michal@svorc.sk>
2020-01-19 18:29:35 +0100

    Remove empty host mount directories, add mkdir to run.sh script

--- run.sh
+++ run.sh
@@ -2,12 +2,21 @@
 # Author: Michal Svorc <michalsvorc.com>
 # Run docker container with predefined mount directories
 
-# Declare variables
+# Image variables
 image_repository='michalsvorc'
 image_name='gimp'
 image_tag=$(git describe --tags --abbrev=0)
 user_name=$image_name
+
+# Mount variables
 mount_path="${PWD}"
+dir_workspace_host="${mount_path}/workspace"
+dir_profile_host="${mount_path}/profile"
+dir_workspace_container="/home/$user_name/workspace"
+dir_profile_container="/home/$user_name/.config/GIMP/$image_tag"
+
+# Create host mount directories
+mkdir -p $dir_workspace_host $dir_profile_host
 
 # Run
 docker run \
@@ -15,7 +24,7 @@ docker run \
     --rm \
     --env DISPLAY=$DISPLAY \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
-    --mount type=bind,source=$mount_path/workspace,target=/home/$user_name/workspace \
-    --mount type=bind,source=$mount_path/profile,target=/home/$user_name/.config/GIMP/$image_tag \
+    --mount type=bind,source=$dir_workspace_host,target=$dir_workspace_container \
+    --mount type=bind,source=$dir_profile_host,target=$dir_profile_container \
     --name $image_repository-$image_name-$image_tag \
     $image_repository/$image_name:$image_tag
