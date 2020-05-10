3dac4b0e31688aca5eddf3bc3e0284183afa7a98
Michal Svorc <michal@svorc.sk>
2020-05-10 12:58:06 +0200

    Fix container_name string replacement expression

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@
 # Docker arguments
 image_name='michalsvorc/gimp'
 image_tag=$(git describe --tags --abbrev=0)
-container_name="${image_name//\//-}-${image_tag}"
+container_name="${image_name//\//_}-${image_tag}"
 mount_profile_source="${PWD}/profile"
 mount_profile_target="/home/user/.config/GIMP/$image_tag"
 mount_workspace_source="${PWD}/workspace"
